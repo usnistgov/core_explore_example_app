b1e6e019822c85e49aca044b6949a25198a0bb30
Philippe Dessauw <philippe.dessauw@nist.gov>
2019-06-14 18:09:41 -0400

    fix: remove print + cleanup code
    Change-Id: I0bfacd68d6a6da16853b948b99a6b3bc3eac6577

--- core_explore_example_app/permissions/discover.py
+++ core_explore_example_app/permissions/discover.py
@@ -1,15 +1,18 @@
-""" Discover rules for core explore example app
+""" Discover rules for core explore example app.
 """
-from __future__ import print_function
+import logging
+
 from core_main_app.permissions import rights as main_rights
 from core_explore_example_app.permissions import rights as explore_example_rights
 
+logger = logging.getLogger(__name__)
+
 
 def init_permissions(apps):
-    """Initialization of groups and permissions
+    """ Initialization of groups and permissions.
 
-    Returns:
-
+    Args:
+        apps (Apps): List of applications to init
     """
     try:
         group = apps.get_model("auth", "Group")
@@ -19,13 +22,19 @@ def init_permissions(apps):
         default_group, created = group.objects.get_or_create(name=main_rights.default_group)
 
         # Get explore example permissions
-        explore_access_perm = permission.objects.get(codename=explore_example_rights.explore_example_access)
-        explore_save_query_perm = permission.objects.get(codename=explore_example_rights.explore_example_save_query)
-        explore_delete_query_perm = permission.objects.get(codename=explore_example_rights.explore_example_delete_query)
+        explore_access_perm = permission.objects.get(
+            codename=explore_example_rights.explore_example_access
+        )
+        explore_save_query_perm = permission.objects.get(
+            codename=explore_example_rights.explore_example_save_query
+        )
+        explore_delete_query_perm = permission.objects.get(
+            codename=explore_example_rights.explore_example_delete_query
+        )
 
-        # add permissions to default group
+        # Add permissions to default group
         default_group.permissions.add(explore_access_perm,
                                       explore_save_query_perm,
                                       explore_delete_query_perm)
     except Exception as e:
-        print('ERROR : Impossible to init the permissions : ' + str(e))
+        logger.error("Impossible to init explore_example permissions: %s" % str(e))
