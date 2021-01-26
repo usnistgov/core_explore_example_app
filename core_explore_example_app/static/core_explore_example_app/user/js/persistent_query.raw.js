75de9ff5124568f93686da463f192a284d57259d
Bouhanni <hnb8@pn120373.campus.nist.gov>
2021-01-26 10:53:05 -0500

    feat: add persistent rename query url
    Change-Id: I0e4bb60550e3ee825945cb379226c4a2d0680035

--- core_explore_example_app/static/core_explore_example_app/user/js/persistent_query.raw.js
+++ core_explore_example_app/static/core_explore_example_app/user/js/persistent_query.raw.js
@@ -1,1 +1,2 @@
 let persistentQueryUrl = "{% url 'core_explore_example_get_persistent_query_url' %}";
+let persistentRenameQueryUrl = "{% url 'core_explore_example_app_rest_persistent_query_example_detail' 'queryId'  %}";
